7cff418ce0edfbe5d1a7c3429b5533a72a9ed34f
DevBacega <dev.victor.bacega@gmail.com>
2021-01-27 01:49:03 -0300

    Developed provider for sending e-mail in development with Ethereal

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,14 +1,15 @@
 import { container } from 'tsyringe';
 import IStorageProvider from './StorageProviders/models/IStorageProvider';
 import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
+
 import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider,
 );
-
-// container.registerSingleton<IMailProvider>(
-//     'MailProvider'
-
-// )
+container.registerInstance<IMailProvider>(
+    'MailProvider',
+    new EtherealMailProvider(),
+);
